d0b31458dba76c78544dec43b2c3d2317acfbbb0
Griffith <griffithtp@gmail.com>
2021-03-01 22:24:30 +0000

    add post, put, delete webclient methods

--- src/main/java/com/learnreactivespring/itemclient/controller/ItemClientController.java
+++ src/main/java/com/learnreactivespring/itemclient/controller/ItemClientController.java
@@ -1,10 +1,11 @@
 package com.learnreactivespring.itemclient.controller;
 
 import com.learnreactivespring.itemclient.domain.Item;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ItemClientController {
@@ -25,4 +26,61 @@ public class ItemClientController {
                 .exchangeToFlux(clientResponse -> clientResponse.bodyToFlux(Item.class))
                 .log("Items in client project exchange: ");
     }
+
+
+    @GetMapping("/client/retrieve/item")
+    public Mono<Item> getItemUsingRetrieve() {
+
+        String id = "ABC";
+
+        return webClient.get().uri("/v1/items/{id}", id)
+                .retrieve()
+                .bodyToMono(Item.class)
+                .log("Single Item in client project");
+    }
+
+    @GetMapping("/client/exchange/item")
+    public Mono<Item> getItemUsingExchange() {
+
+        String id = "ABC";
+
+        return webClient.get().uri("/v1/items/{id}", id)
+                .exchangeToMono(clientResponse -> clientResponse.bodyToMono(Item.class))
+                .log("Single Item in client project exchange: ");
+    }
+
+    @PostMapping("/client/createItem")
+    public Mono<Item> createItem(@RequestBody Item item) {
+
+        Mono<Item> itemMono = Mono.just(item);
+
+        return webClient.post().uri("/v1/items")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(itemMono, Item.class)
+                .retrieve()
+                .bodyToMono(Item.class)
+                .log("created Item");
+    }
+
+    @PutMapping("/client/item/{id}")
+    public Mono<Item> updateItem(@PathVariable String id, @RequestBody Item item) {
+
+        Mono<Item> itemBody = Mono.just(item);
+
+        return webClient.put().uri("/v1/items/{id}", id)
+                .body(itemBody, Item.class)
+                .retrieve()
+                .bodyToMono(Item.class)
+                .log("Updated Item is: ");
+    }
+
+    @DeleteMapping("/client/item/{id}")
+    public Mono<Void> deleteItem(@PathVariable String id) {
+
+        return webClient.delete().uri("/v1/items/{id}", id)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .log("Deleted item is: ");
+    }
+
 }
